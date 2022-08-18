f0c6fa627fd09f91304dfcb723bb2cbfae6cb73b
id39 <100359834+idme520@users.noreply.github.com>
2022-08-18 20:40:38 +0800

    Add files via upload

---
+++ 2.2.js
@@ -0,0 +1,9 @@
+function ddd() {
+  /*控制按钮部分*/
+  let aba = document.getElementById("fff");
+  if (aba.style.display == "none") {
+    aba.style.display = "block";
+  } else {
+    aba.style.display = "none";
+  }
+}
